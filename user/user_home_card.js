8284bd1adbee44562b372e16d280f67670766dfb
Amitesh Ranjan <amiranja4@publicisgroupe.net>
2019-04-10 14:30:06 +0530

    Dynamic added cards using javascript for user side. Only details part left

--- user/user_home_card.js
+++ user/user_home_card.js
@@ -1,22 +1,38 @@
 
-// for (var i = 0; i < 3; i++) {
-//     var card = `<div class="card">
-//         <img class="imgr" src=${devicedata[i].img} alt="Avatar" style="width:100%; ">
-//         <div class="container">
-//           <h4>${devicedata[i].name}</h4>
-//           <p>${devicedata[i].brand}</p>
-//           <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModalCenter">
-//             Extend
-//           </button>
-//           <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModalCenter1">
-//             Return
-//           </button>
-//           <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModalCenter4">
-//             Lost
-//           </button>
-//         </div>
-//       </div>`;
-//     document.getElementById("currently_allocated_devices").appendChild('card');
-// }
+for (var i = 0; i < 3; i++) {
+    var card1 = `<div class="card">
+        <img class="imgr" src=${devicedata[i].img} alt="Avatar" style="width:100%; ">
+        <div class="container">
+          <h4>${devicedata[i].name}</h4>
+          <p>${devicedata[i].brand}</p>
+          <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModalCenter">
+            Extend
+          </button>
+          <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModalCenter1">
+            Return
+          </button>
+          <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModalCenter4">
+            Lost
+          </button>
+        </div>
+      </div>`;
+      document.getElementById("currently_allocated_devices").innerHTML += card1;
+}
+
+    for(var i=4; i<6; i++){
+      var card2 = ` <div class="card">
+      <img class="imgr" src=${devicedata[i].img} alt="Avatar" style="width:100%; ">
+      <div class="container">
+        <h4>${devicedata[i].name}</h4>
+        <p>${devicedata[i].brand}</p>
+        <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModalCenter1">
+          Cancel request
+        </button>
+      </div>
+    </div>`;
+    document.getElementById("currently_requested_devices").innerHTML += card2;
+    
+    
+}
 
 
