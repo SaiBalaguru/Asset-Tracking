dac36a4f377d2befdab2303452ad491ac731018e
Bhavika Jain <bhavikajain@MacBook-Pro.local>
2019-04-11 12:48:15 +0530

    adding

--- user/user_name.js
+++ user/user_name.js
@@ -1,7 +1,7 @@
 var name = sessionStorage.getItem('name');
 console.log(name);
 var hello = document.getElementById('hello');
-document.getElementById('hello').innerHTML = "   Hello "+name;
+//document.getElementById('hello').innerHTML = "   Hello "+name;
 document.getElementById('name_table').innerHTML = name;
 
 var req = document.getElementsByClassName('btn btn-dark request');
