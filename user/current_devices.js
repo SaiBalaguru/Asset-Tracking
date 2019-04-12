12c9b39eb2a10b454a0ff318909c7835f7396ca0
Bhavika Jain <bhavikajain@MacBook-Pro.local>
2019-04-12 09:26:15 +0530

    changes

--- user/current_devices.js
+++ user/current_devices.js
@@ -1,17 +1,17 @@
-    var card = `<div class="card">
-
+var card = `<div class="card">
+ 
      <img class="imgr" src="${localStorage.image}" alt="${localStorage.brand} ${localStorage.name}" style="width:100%; ">
     <div class="container">
       <h4>${localStorage.name}</h4>
       <p>${localStorage.brand}</p>
       <p>${localStorage.os}</p>
-      <button type="button" class="btn btn-dark normal" style:"margin-left:25%" >Approve</button>
-      <button type="button" class="btn btn-dark request" style:"margin-left:25%" > Decline</button>
+      <button type="button" class="btn btn-dark normal" style:"margin-left:25%" >Status</button>
+      <button type="button" class="btn btn-dark request" style:"margin-left:25%" > Cancel</button>
       <div id="requested">
       </div>
       </div>
   </div>`;
-    document.getElementById('normal_requested_devices').innerHTML += card;
+    document.getElementById('current_devices').innerHTML += card;
   // var req = document.getElementById('btn btn-dark request');
   // req.onclick = function(){
   //   localStorage.setItem("device_name",device.name);
@@ -20,6 +20,4 @@
   // }
   i++;
   
-
-
 
--- user/user_devices_card.js
+++ user/user_devices_card.js
@@ -41,7 +41,41 @@ devicedata.forEach(function (device, index) {
   })
  }
 
+
+//  function approved_item(element){
+//   console.log(element.id);
+//   document.getElementById('approved').innerHTML = "Device Approved";
+//  devicedata.forEach(function(device){
+//    //console.log(device)
+//    if(device.approved == element.id){
+//      console.log(device.approved);
+//      localStorage.setItem("id",device.id);
+//      localStorage.setItem("name",device.name);
+//      localStorage.setItem("image",device.img);
+//      localStorage.setItem("brand",device.brand);
+//      localStorage.setItem("os",device.os);
+//    }
+//  })
+// }
  
+function myFunction() {
+  var input, filter, cards, cardContainer, h5, title,title1,title2, i;
+  input = document.getElementById("myFilter");
+  filter = input.value.toUpperCase();
+
+  cardContainer = document.getElementById("myItems");
+  cards = cardContainer.getElementsByClassName("card");
+  for (i = 0; i < cards.length; i++) {
+    title = devicedata[i].name;
+    title1 = devicedata[i].brand;
+    title2 = devicedata[i].os;
+    if (title.toUpperCase().indexOf(filter) > -1 || title1.toUpperCase().indexOf(filter) > -1||title2.toUpperCase().indexOf(filter) > -1) {
+      cards[i].style.display = "";
+    } else {
+      cards[i].style.display = "none";
+    }
+  }
+}
 
 //  var request_device = document.getElementsByClassName('btn btn-dark normal');
 //  console.log(request_device);
